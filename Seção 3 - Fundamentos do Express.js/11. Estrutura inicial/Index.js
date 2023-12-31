6175979c115248201da5cc6f63e8dde158097a7b
Thiago Leandro <tiago14789@live.com>
2023-12-31 16:38:37 -0300

    Adicionando parametros

--- Seção 3 - Fundamentos do Express.js/11. Estrutura inicial/Index.js
+++ Seção 3 - Fundamentos do Express.js/11. Estrutura inicial/Index.js
@@ -6,12 +6,28 @@ app.get("/",function(req,res){
     res.send("Bem vindo ao guia do Programador")
 });
 
-app.get("/blog",function(req,res){
-    res.send("Bem vindo ao meu blog")
+// :artigo? não é um parametro obrigatorio  
+app.get("/blog/:artigo?",function(req,res){
+
+    var artigo = req.params.artigo;
+
+    if(artigo){
+        res.send("<h2>Artigo: " + artigo + " </h2>");
+    }else{
+        res.send("Bem vindo ao meu blog")
+    }
 });
 
 app.get("/canal/youtube",function(req,res){
     res.send("Bem vindo ao meu canal")
+});
+
+app.get("/ola/:nome/empresa", function(req,res){
+    //req são os dados ernviados pelo usuario
+    //res é a resposta é a resposta que vai ser enviada para o usuario
+    var nome = req.params.nome;
+    var empresa = req.params.empresa;
+    res.send("<h1>Oi! " + nome + " do " + empresa + " </h1>");
 });
 
 app.listen(4000,function(erro){
