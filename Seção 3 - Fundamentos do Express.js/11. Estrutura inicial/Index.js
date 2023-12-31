354e8bfe9f344c31bd90534ec6813819c8ec17aa
Thiago Leandro <tiago14789@live.com>
2023-12-31 15:23:51 -0300

    Rotas no site

--- Seção 3 - Fundamentos do Express.js/11. Estrutura inicial/Index.js
+++ Seção 3 - Fundamentos do Express.js/11. Estrutura inicial/Index.js
@@ -2,6 +2,18 @@ const { error } = require("console");
 const express = require ("express"); //importando o Express
 const app = express(); //Iniciando o Express
 
+app.get("/",function(req,res){
+    res.send("Bem vindo ao guia do Programador")
+});
+
+app.get("/blog",function(req,res){
+    res.send("Bem vindo ao meu blog")
+});
+
+app.get("/canal/youtube",function(req,res){
+    res.send("Bem vindo ao meu canal")
+});
+
 app.listen(4000,function(erro){
     if(erro){
         console.log("Ocorreu um erro");
